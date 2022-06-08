5ec73f772b27bc765b1c912c50f1726819ffdbff
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-08 15:13:21 +0200

    starting every class but without using Interfaces yet

---
+++ src/Services/RentalService.java
@@ -0,0 +1,36 @@
+package Services;
+
+import entities.CarRental;
+import entities.Invoice;
+
+public class RentalService {
+    private double pricePerDay;
+    private double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+
+    public RentalService(double pricePerDay, double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+
+        double hours = ((double) t2 - t1) / (1000 * 60 * 60);
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment,tax));
+    }
+
+
+}
--- src/course/Main.java
+++ src/course/Main.java
@@ -1,11 +1,48 @@
 package course;
 
+import Services.BrazilTaxService;
+import Services.RentalService;
+import entities.CarRental;
+import entities.Vehicle;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+
+        System.out.println("Enter rental data");
+        System.out.println("Car model: ");
+        String carModel =sc.nextLine();
+        System.out.print("Pickup (dd/MM/yyyy hh:ss): ");
+        Date start = sdf.parse(sc.nextLine());
+        System.out.print("Return (dd/MM/yyyy hh:ss):  ");
+        Date finish = sdf.parse(sc.nextLine());
+
+        CarRental carRental = new CarRental(start,finish, new Vehicle(carModel));
+
+        System.out.print("Enter price per hour ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Enter price per day ");
+        double pricePerDay = sc.nextDouble();
+        RentalService rentalService = new RentalService(pricePerDay,pricePerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
+
+        System.out.println("Invoice");
+        System.out.println("Basic payment" + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f",carRental.getInvoice().getTax()));
+        System.out.println("Total payment: " + String.format("%.2f",carRental.getInvoice().getTotalPayment()));
+
+        sc.close();
 
 
-
-	// write your code here
     }
 }
